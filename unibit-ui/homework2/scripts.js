ac751b3008210d75f29f56d7a4612cb3ebbe590d
Kosta <k.v.dinkov@gmail.com>
2021-02-04 14:36:03 +0200

    update: homework 2 for unibit-ui

--- unibit-ui/homework2/scripts.js
+++ unibit-ui/homework2/scripts.js
@@ -1,13 +1,24 @@
+
+// Използвам масив от имена на изображения
 const photos = ["1.jpg","2.jpg","3.jpg", "4.jpg","5.jpg"];
 const baseImgPath = "img/";
 
+// задаваме началното изображение да е нулевият индекс от масива
+// като променяме src атрибута на img елемента в html файла.
 const imgElement = document.getElementById("imgHolder");
 imgElement.src = baseImgPath + photos[0];
 
-
 const nav = document.querySelector(".navigation");
+
+// за всеки елемент от масива създаваме span елемент,
+// който стилизираме (в styless.css) да изглежда като сиво кръгче
+// на което задаваме onclik хендлъра да изпълнява функция, която сменя
+// текущото изображение, към което соки img елемента
 for (let i = 0; i < photos.length; i++) {
   const circle = createDotNav(photos[i],imgElement);
+  // по подразбиране, първият елемент ще бъде с клас active
+  if(i==0)circle.setAttribute("class","active");
+  
   nav.appendChild(circle);
 }
 
@@ -17,12 +28,11 @@ function createDotNav(url, imgElement){
   circle.onclick = (event)=>{
     imgElement.src = baseImgPath + url;
     const circles = document.querySelectorAll(".navigation span");
-
-    //clear active class from rest of the circles
+    //премахваме "active" класа от другите кръгчета
     circles.forEach(e=>{e.setAttribute("class","")})
+    
+    //задаваме "active" клас на кръгчето, което сме "кликнали"
     event.target.setAttribute("class","active");
   }
-
   return circle;
-
 }
