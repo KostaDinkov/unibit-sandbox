333b1d564ef9588c6bc39a1a90a29a0daa966a02
Kosta <k.v.dinkov@gmail.com>
2021-02-22 14:49:42 +0200

    update sandbox

---
+++ unibit-oop/PP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ unibit-oop/sandbox/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Circle s = new Circle();
+            Rect r = new Rect();
+            Ball b = new Ball();
+
+
+            IPrintArea [] printables = new IPrintArea[] {s, r, b};
+
+            
+
+            
+        }
+    }
+
+    public abstract class Shape
+    {
+        
+
+        public void PrintPerimeter()
+        {
+
+            Console.WriteLine("Perimeter ...");
+        }
+    }
+
+    public class Rect : Shape, IPrintArea
+    {
+        public void PrintRect()
+        {
+
+        }
+
+        public void PrintArea()
+        {
+            
+        }
+
+    }
+
+    public class Circle : Shape, IPrintArea
+    {
+        public void PrintCirc()
+        {
+
+        }
+
+        public void PrintArea()
+        {
+            
+        }
+    }
+
+
+    public class Ball : IPrintArea
+    {
+
+        public void PrintArea()
+        {
+
+        }
+    }
+
+    interface IPrintArea
+    {
+
+        public void PrintArea();
+
+
+    }
+
+
+
+}
